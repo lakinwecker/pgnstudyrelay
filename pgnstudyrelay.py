768f769af579fa5085556f74d667aa53f11b98a1
Lakin Wecker <lakin@structuredabstraction.com>
2017-06-24 14:41:10 -0600

    Create chapter immediately after getting it.

--- pgnstudyrelay.py
+++ pgnstudyrelay.py
@@ -81,25 +81,23 @@ class PGNStudyRelay:
             chapter_lookup = {game_key_from_chapter(c): c for c in self.study.get_chapters()}
             chapter = chapter_lookup.get(game.key)
 
-            should_sync = False
+            if not chapter:
+                print("++ [SYNCING] inserting new chapter for: {}".format(game.title))
+                await self.study.create_chapter_from_pgn(str(game))
+                chapters_created = True
+                continue
 
+            # Do some checks before we other syncing.
+            should_sync = False
             old_version = self.chapter_versions_by_key[game.key]
             if old_version != chapter['version']:
                 should_sync = True
-
-            # Only process the PGN if it's different from last time
             old_game = self.pgns_by_key[game.key]
             if str(old_game) != str(game):
                 should_sync = True
             self.pgns_by_key[game.key] = game
 
             if not should_sync:
-                continue
-
-            if not chapter:
-                print("++ [SYNCING] inserting new chapter for: {}".format(game.title))
-                await self.study.create_chapter_from_pgn(str(game))
-                chapters_created = True
                 continue
 
             # This could happen above, but then that delays the creation of the 
